60f3ce15e26a71c1e70c62b1ef59af8beb2d72d5
HarithaRugada <haritharugada@gmail.com>
2020-05-15 17:49:01 +0530

    added usecase9

--- src/com/empwage/test/IEmployeeWage.java
+++ src/com/empwage/test/IEmployeeWage.java
@@ -28,6 +28,16 @@ public class IEmployeeWage
                 abcmanageemployee.EmployeeParttime(employee);
                 abcmanageemployee.EmployeeMonthWage(employee);
 
+		System.out.println("<<<<<<<<<<<<<<----------------------------------------------------------->>>>>>>>>>>>>>");
+
+		EmpWageBuilder reliance=new EmpWageBuilder("Reliance" , 20, 5 , 50);
+		EmpWageBuilder bigc=new EmpWageBuilder("Big C" , 15, 10 , 30);
+		reliance.computeEmpWage();
+		//System.out.println(reliance);
+		bigc.computeEmpWage();
+		//System.out.println(bigc);
+
+
         }
 
 }
