62cd93cbfa608a672b9c0f76b92105f09fc40645
HarithaRugada <haritharugada@gmail.com>
2020-05-16 12:51:06 +0530

    added usecase5

--- src/com/empwage/test/EmployeeWage.java
+++ src/com/empwage/test/EmployeeWage.java
@@ -15,36 +15,48 @@ public class EmployeeWage
 		Employee employee=new Employee(20,(int)(Math.random()*10));
                 ManageEmployee manageemployee=new ManageEmployee();
 		Scanner Sc=new Scanner(System.in);
-		System.out.println("Enter your choice");
-		System.out.println("1.Check whether employee is present or absent");
-		System.out.println("2.Get the daily wage of the employee");
-		System.out.println("3.Check whether the employee is working parttime or fulltime");
-		System.out.println("4.Calculate the monthly wage");
-		int choice=Sc.nextInt();
-		switch(choice)
+
+		while(true)
 		{
-			case 1:
-				manageemployee.EmployeePresent(employee);
-				break;
 
-			case 2:
-				manageemployee.EmployeePresent(employee);
-				manageemployee.EmployeeDailyWage(employee);
-				break;
+			System.out.println("Enter your choice");
+			System.out.println("1.Check whether employee is present or absent");
+			System.out.println("2.Get the daily wage of the employee");
+			System.out.println("3.Check whether the employee is working parttime or fulltime");
+			System.out.println("4.Calculate the monthly wage");
+			System.out.println("5.Exit");
+			int choice=Sc.nextInt();
+			switch(choice)
+			{
+				case 1:
+					manageemployee.EmployeePresent(employee);
+					break;
 
-			case 3:
-				manageemployee.EmployeePresent(employee);
-				manageemployee.EmployeeParttime(employee);
-				break;
+				case 2:
+					manageemployee.EmployeePresent(employee);
+					manageemployee.EmployeeDailyWage(employee);
+					break;
 
-			case 4:
-				manageemployee.EmployeeMonthWage(employee);
-				break;
+				case 3:
+					manageemployee.EmployeePresent(employee);
+					manageemployee.EmployeeParttime(employee);
+					break;
 
-			default:
+				case 4:
+					manageemployee.EmployeeMonthWage(employee);
+					break;
+
+				case 5:
+					System.exit(0);
+					break;
+
+				default:
 				System.out.println("Enter valid choice");
 
+			}
+
 		}
+
 	}
 
 }
